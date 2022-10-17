a6ab43cf6d7dde5f2764dcc33fde78b689480746
vaseehar <vaseehar93@gmail.com>
2022-10-18 01:21:28 +0530

    1

---
+++ src/stack/LCValidParanthesis.java
@@ -0,0 +1,31 @@
+package stack;
+
+import java.util.Stack;
+
+/*https://leetcode.com/problems/valid-parentheses/*/
+
+public class LCValidParanthesis {
+
+	public static void main(String[] args) {
+		System.out.println(isValid("{}()[]()"));
+	}
+
+	public static boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+		if (s.length() % 2 != 0) {
+			return false;
+		}
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.push(')');
+			} else if (c == '{') {
+				stack.push('}');
+			} else if (c == '[') {
+				stack.push(']');
+			} else if (stack.isEmpty() || stack.pop() != c) {
+				return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+}
