1800ba2b7ce1a3ab65705ea647de4c166032ef50
vaseehar <vaseehar93@gmail.com>
2022-10-29 11:06:13 +0530

    29-10

---
+++ src/binaryTree/SameTree.java
@@ -0,0 +1,43 @@
+package binaryTree;
+
+
+/*https://leetcode.com/problems/same-tree/
+*/
+public class SameTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+		 if(p==null && q==null) {
+	            return true;
+	        }
+	        if(p==null||q==null || p.val!=q.val) {
+	            return false;
+	        }
+	        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+	}
+
+	
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
