9fb1f814a60c6901b1dd74ee79975eef4a95294c
vaseehar <vaseehar93@gmail.com>
2022-11-03 12:01:43 +0530

    03-11

---
+++ src/binaryTree/SymmetricTree.java
@@ -0,0 +1,46 @@
+package binaryTree;
+
+/*https://leetcode.com/problems/symmetric-tree/
+*/
+public class SymmetricTree {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+		return root == null || isMirror(root.left, root.right);
+
+	}
+
+	boolean isMirror(TreeNode left, TreeNode right) {
+		if (left == null && right == null)
+			return true;
+
+		if (left == null || right == null)
+			return false;
+
+		if (left.val != right.val)
+			return false;
+		return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
