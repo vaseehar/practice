f9b3e2940b5cc15d0c6d78c5c845a0f36d36d7aa
vaseehar <vaseehar93@gmail.com>
2022-11-07 12:19:43 +0530

    07-11-22

---
+++ src/arrays/PascalTriangle2_119.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*https://leetcode.com/problems/pascals-triangle-ii/submissions/
+*/
+public class PascalTriangle2_119 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static List<Integer> generate1(int rowIndex) {
+
+		List<Integer> row = null, pre = null;
+		for (int i = 0; i <= rowIndex; ++i) {
+			row = new ArrayList<Integer>();
+			for (int j = 0; j <= i; ++j)
+				if (j == 0 || j == i)
+					row.add(1);
+				else
+					row.add(pre.get(j - 1) + pre.get(j));
+			pre = row;
+		}
+		return row;
+
+	}
+
+}
