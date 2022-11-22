aac403cc162aa44fe1ec0a8ca6dd2166c758da4a
vaseehar <vaseehar93@gmail.com>
2022-11-22 10:09:31 +0530

    22-11-22

---
+++ src/arrays/MajorityElement_169.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MajorityElement_169 {
+
+	public static void main(String[] args) {
+		System.out.println(majorityElement(new int[] { 1, 2, 3}));
+	}
+
+	public static int majorityElement(int[] nums) {
+		Arrays.sort(nums);
+
+		return nums[(nums.length - 1) / 2];
+	}
+
+}
