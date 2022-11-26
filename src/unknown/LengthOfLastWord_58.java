372055a39dc8d9a777ea58af67cfdabd0b327773
vaseehar <vaseehar93@gmail.com>
2022-11-26 11:36:59 +0530

    26-11-22

---
+++ src/unknown/LengthOfLastWord_58.java
@@ -0,0 +1,18 @@
+package unknown;
+
+public class LengthOfLastWord_58 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int lengthOfLastWord(String s) {
+		s = s.trim();
+		if (s.contains(" ")) {
+			return s.substring(s.lastIndexOf(" ") + 1).length();
+		} else {
+			return s.length();
+		}
+	}
+
+}
