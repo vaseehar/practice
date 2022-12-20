a1c2311e4c8fd7a01c1db950969b0fe6a0c80a5e
vaseehar <vaseehar93@gmail.com>
2022-12-21 00:31:25 +0530

    21-12-22

---
+++ src/unknown/GuessGame.java
@@ -0,0 +1,14 @@
+package unknown;
+
+public class GuessGame {
+
+	public static int guess(int num) {
+		int pick = 6;
+		if (num > pick)
+			return -1;
+		else if (num < pick)
+			return 1;
+		else
+			return 0;
+	}
+}
