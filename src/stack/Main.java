56b141cae7d17c97e77b1ad75001bb77a4b0511e
vaseehar <vaseehar93@gmail.com>
2022-12-27 23:11:19 +0530

    27-12-22

---
+++ src/stack/Main.java
@@ -0,0 +1,20 @@
+package stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyQueue_232 myQueue_232 = new MyQueue_232();
+		myQueue_232.push(1);
+		myQueue_232.push(2);
+		myQueue_232.push(3);
+		myQueue_232.push(4);
+		System.out.println(myQueue_232.pop());
+		myQueue_232.push(5);
+		System.out.println(myQueue_232.pop());
+		System.out.println(myQueue_232.pop());
+		System.out.println(myQueue_232.pop());
+		System.out.println(myQueue_232.pop());
+		
+	}
+
+}
