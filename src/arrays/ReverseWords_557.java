60eb768c233d6b1211a2bb89d2b303d45699f209
vaseehar <vaseehar93@gmail.com>
2022-12-28 08:10:27 +0530

    28-12-22

---
+++ src/arrays/ReverseWords_557.java
@@ -0,0 +1,40 @@
+package arrays;
+
+public class ReverseWords_557 {
+
+	public static void main(String[] args) {
+		new ReverseWords_557().reverseWords1("Let's take LeetCode contest");
+	}
+
+// faster than 38%
+	public String reverseWords(String s) {
+		String[] sArr = s.split(" ");
+		String finalStr = "";
+		for (int i = 0; i < sArr.length; i++) {
+			char[] c = sArr[i].toCharArray();
+			for (int j = 0, k = c.length - 1; j < k; j++, k--) {
+				char temp = c[j];
+				c[j] = c[k];
+				c[k] = temp;
+			}
+			finalStr = finalStr + new String(c) + " ";
+		}
+		return finalStr.trim();
+	}
+
+//copied from discussion , faster than 85%
+	public String reverseWords1(String s) {
+		StringBuilder st = new StringBuilder();
+		String[] StringArr = s.split(" ");
+		for (int i = 0; i < StringArr.length; i++) {
+			char[] a = StringArr[i].toCharArray();
+			int aLength = a.length - 1;
+			while (aLength >= 0) {
+				st.append(a[aLength--]);
+			}
+			st.append(" ");
+		}
+		return st.toString().trim();
+	}
+
+}
