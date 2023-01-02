4d464d914c6e640ff2339137d3e97d9ce3e3f7c6
vaseehar <vaseehar93@gmail.com>
2023-01-02 10:21:57 +0530

    2-1-23

---
+++ src/recursion/PowerOf3_326.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class PowerOf3_326 {
+
+	public static void main(String[] args) {
+		System.out.println(new PowerOf3_326().isPowerOfThree(27));
+	}
+
+	public boolean isPowerOfThree(int n) {
+		if (n == 3 || n == 1)
+			return true;
+		if (n == 0 || n % 3 != 0)
+			return false;
+		return isPowerOfThree(n / 3);
+	}
+
+}
