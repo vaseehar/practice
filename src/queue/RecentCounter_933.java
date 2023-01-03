dc48c34bf7bec8b27a8ba1a246c591503db5c4af
vaseehar <vaseehar93@gmail.com>
2023-01-03 10:05:50 +0530

    3-1-23

---
+++ src/queue/RecentCounter_933.java
@@ -0,0 +1,31 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter_933 {
+
+	Queue<Integer> queue;
+
+	public RecentCounter_933() {
+		queue = new LinkedList<>();
+	}
+
+	public int ping(int t) {
+		queue.add(t);
+		while (!queue.isEmpty() && t - queue.peek() > 3000) {
+			queue.poll();
+		}
+
+		return queue.size();
+	}
+
+	public static void main(String[] args) {
+		RecentCounter_933 recentCounter_933 = new RecentCounter_933();
+		System.out.println(recentCounter_933.ping(1));
+		System.out.println(recentCounter_933.ping(100));
+		System.out.println(recentCounter_933.ping(3001));
+		System.out.println(recentCounter_933.ping(3002));
+	}
+
+}
