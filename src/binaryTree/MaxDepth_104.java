8f949e8a97f847e7d7147b4d87649fecb2fa51fc
vaseehar <vaseehar93@gmail.com>
2023-01-04 23:07:08 +0530

    04-01-23

---
+++ src/binaryTree/MaxDepth_104.java
@@ -0,0 +1,39 @@
+package binaryTree;
+
+public class MaxDepth_104 {
+
+	public static void main(String[] args) {
+		TreeNode node = new TreeNode(1);
+		node.left = new TreeNode(2);
+		node.right = new TreeNode(3);
+		node.right.right = new TreeNode(4);
+		node.right.left = new TreeNode(5);
+		node.right.left.left = new TreeNode(5);
+		System.out.println(new MaxDepth_104().maxDepth(node));
+	}
+
+	public int maxDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
