1ee0243c01ba270c13eb7d8e03609aa3505c5c45
vaseehar <vaseehar93@gmail.com>
2023-01-06 18:22:17 +0530

    6-1-23

---
+++ src/recursion/PowerOfFour_342.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class PowerOfFour_342 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isPowerOfFour(int n) {
+
+		if (n == 1) {
+			return true;
+		}
+
+		if (n % 4 != 0 || n == 0) {
+			return false;
+		}
+
+		return isPowerOfFour(n / 4);
+	}
+
+}
