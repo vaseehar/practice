523eefca1bcdb3355b56407a404653f420833b31
vaseehar <vaseehar93@gmail.com>
2023-01-31 10:17:37 +0530

    31-1-23

---
+++ src/stack/FlattenBinaryTree_114.java
@@ -0,0 +1,52 @@
+package stack;
+
+import java.util.Stack;
+
+public class FlattenBinaryTree_114 {
+
+	public static void main(String[] args) {
+
+	}
+
+	Stack<TreeNode> stack = new Stack<>();
+
+	public void flatten(TreeNode root) {
+		if(root == null) return;
+		pushToStack(root);
+		TreeNode node ;
+		while(!stack.isEmpty()) {
+			node = stack.pop();
+			node.left = null;
+			if(!stack.isEmpty())
+			node.right = stack.peek();
+		}
+	}
+
+	public void pushToStack(TreeNode node) {
+		if (node == null)
+			return;
+		pushToStack(node.right);
+		pushToStack(node.left);
+		stack.push(node);
+		
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
