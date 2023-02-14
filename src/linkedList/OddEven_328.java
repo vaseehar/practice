ca934b9514e38f5decc7facca7a7a7b263a3d4cc
vaseehar <vaseehar93@gmail.com>
2023-02-14 17:18:30 +0530

    14-2-23

---
+++ src/linkedList/OddEven_328.java
@@ -0,0 +1,44 @@
+package linkedList;
+
+import linkedList.SwapNodesInPairs_24.ListNode;
+
+public class OddEven_328 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public ListNode oddEvenList(ListNode head) {
+		if(head==null) return null;
+		if (head.next == null)
+			return head;
+		ListNode evenNodeFirst = head.next, oddNode = head, evenNode = head.next, lastOddNode = oddNode;
+		while (oddNode != null && oddNode.next != null && evenNode != null && evenNode.next != null) {
+			oddNode.next = oddNode.next.next;
+			oddNode = oddNode.next;
+			lastOddNode = oddNode;
+			evenNode.next = evenNode.next.next;
+			evenNode = evenNode.next;
+		}
+		lastOddNode.next = evenNodeFirst;
+		return head;
+	}
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
