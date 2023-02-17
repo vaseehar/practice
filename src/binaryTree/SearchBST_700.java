29d80a2e52abac5dd4419b0c538688c5eed68ee5
vaseehar <vaseehar93@gmail.com>
2023-02-17 23:17:43 +0530

    17-2-23

---
+++ src/binaryTree/SearchBST_700.java
@@ -0,0 +1,37 @@
+package binaryTree;
+
+public class SearchBST_700 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public TreeNode searchBST(TreeNode root, int val) {
+		TreeNode node = root;
+		while (node != null) {
+			if (node.val == val)
+				return node;
+		}
+		return null;
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+}
