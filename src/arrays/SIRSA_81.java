407649f9267946b2f98cd1be39af1574d208fd50
vaseehar <vaseehar93@gmail.com>
2023-02-20 10:27:35 +0530

    20-2-23

---
+++ src/arrays/SIRSA_81.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class SIRSA_81 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean search(int[] nums, int target) {
+		if (nums == null)
+			return false;
+		int n = nums.length, i;
+		if (target <= nums[n - 1]) {
+			if (target == nums[n - 1])
+				return true;
+			i = n - 1;
+			while (i > 0 && target < nums[i]) {
+				i--;
+			}
+			return nums[i] == target;
+		} else {
+			if (target == nums[0])
+				return true;
+			i = 0;
+			while (i < n - 1 && target > nums[i])
+				i++;
+			return nums[i] == target;
+		}
+	}
+
+}
