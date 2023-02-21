dcf5f4f5bde9f84efe99aef58eafb18079f3618b
vaseehar <vaseehar93@gmail.com>
2023-02-21 09:57:50 +0530

    21-2-23

---
+++ src/linkedList/RemoveNthNodeFromEnd_19.java
@@ -0,0 +1,55 @@
+package linkedList;
+
+public class RemoveNthNodeFromEnd_19 {
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+//		head.next.next = new ListNode(3);
+//		head.next.next.next = new ListNode(4);
+//		head.next.next.next.next = new ListNode(5);
+		ListNode node = new RemoveNthNodeFromEnd_19().removeNthFromEnd(head, 2);
+		System.out.println(node);
+	}
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if (head == null)
+			return head;
+		ListNode node = head;
+		int size = 0;
+		while (node != null) {
+			node = node.next;
+			size++;
+		}
+		if (size - n == 0) {
+			return head.next;
+		}
+		int i = 1;
+		node = head;
+		while (i < size - n) {
+			node = node.next;
+			i++;
+		}
+		node.next = node.next.next;
+
+		return head;
+	}
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
