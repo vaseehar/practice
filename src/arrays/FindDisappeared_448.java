4bed73cb7acdc72971143dab119d3fc132e6df11
vaseehar <vaseehar93@gmail.com>
2023-03-02 19:58:11 +0530

    2-3-23

---
+++ src/arrays/FindDisappeared_448.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappeared_448 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		int n = nums.length;
+		boolean[] isPresent = new boolean[n + 1];
+		List<Integer> result = new ArrayList<>();
+		for (int a : nums) {
+			isPresent[a] = true;
+		}
+		for (int i = 1; i < n + 1; i++) {
+			if (!isPresent[i])
+				result.add(i);
+		}
+		return result;
+	}
+
+}
