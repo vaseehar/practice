87da3ced1da492bde29ad75abf5d7c60aa163424
vaseehar <vaseehar93@gmail.com>
2023-03-03 11:24:57 +0530

    3-3-23

---
+++ src/arrays/MIRSA_153.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class MIRSA_153 {
+
+	public static void main(String[] args) {
+		System.out.println(new MIRSA_153().findMin(new int[] { 7, 8, 9, 10, 1, 2, 3 }));
+		System.out.println(new MIRSA_153().findMin(new int[] { 3, 4, 5, 1, 2, 2 }));
+		System.out.println(new MIRSA_153().findMin(new int[] { 2, 3, 1, 1, 1, 1 }));
+		System.out.println(new MIRSA_153().findMin(new int[] { 2, 3, 4, 5, 6, 7, -1 }));
+		System.out.println(new MIRSA_153().findMin(new int[] { 2, -1 }));
+		System.out.println(new MIRSA_153().findMin(new int[] { 3, 1, 2 }));
+	}
+
+	public int findMin(int[] nums) {
+		int n = nums.length;
+		if (n == 1 || nums[0] < nums[n - 1])
+			return nums[0];
+		if (n == 2)
+			return Math.min(nums[0], nums[1]);
+		int low = 0, high = n - 1, mid = (high + low) / 2;
+		while (low < mid) {
+			if (nums[mid] > nums[high])
+				low = mid + 1;
+			else if (nums[mid] <= nums[high])
+				high = mid;
+			mid = (high + low) / 2;
+		}
+		return nums[mid] < nums[high] ? nums[mid] : nums[high];
+	}
+
+}
---
+++ src/arrays/NextGreaterElement_503.java
@@ -0,0 +1,83 @@
+package arrays;
+
+import java.util.Stack;
+
+public class NextGreaterElement_503 {
+
+	public static void main(String[] args) {
+		new NextGreaterElement_503().nextGreaterElements1(new int[] { 18, 17, 16, 6, 7, 9 });
+		// new NextGreaterElement_503().nextGreaterElements1(new int[] { 1, 2, 1 });
+		// new NextGreaterElement_503().nextGreaterElements1(new int[] { 3, 4, 2, 1, 0
+		// });
+	}
+
+	public int[] nextGreaterElements(int[] nums) {
+		int n = nums.length;
+		if (n == 1)
+			return new int[] { -1 };
+		int[] nonInc = new int[n], res = new int[n];
+		nonInc[n - 1] = nums[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			if (nums[i] > nonInc[i + 1])
+				nonInc[i] = nums[i];
+			else
+				nonInc[i] = nonInc[i + 1];
+		}
+		for (int i = 0; i < n; i++) {
+			if (nums[i] < nonInc[i]) {
+				int j = i + 1;
+				while (nums[j] <= nums[i]) {
+					j++;
+				}
+				res[i] = nums[j];
+			} else {
+				if (nonInc[0] > nums[i])
+					res[i] = nonInc[0];
+				else
+					res[i] = -1;
+			}
+		}
+		return res;
+	}
+
+//copied from discussion , faster
+	public int[] nextGreaterElements1(int[] nums) {
+
+		Stack<Integer> stack = new Stack<>();
+		for (int i = nums.length - 1; i >= 0; i--) {
+			stack.push(nums[i]);
+		}
+
+		for (int i = nums.length - 1; i >= 0; i--) {
+			int number = nums[i];
+			while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+				stack.pop();
+			}
+
+			nums[i] = stack.empty() ? -1 : stack.peek();
+			stack.push(number);
+		}
+
+		return nums;
+	}
+	//faster , copied from discussion
+	public int[] nextGreaterElements2(int[] nums) {
+		Stack<Integer> stack = new Stack();
+		int[] res = new int[nums.length];
+		int n = nums.length;
+
+		for (int i = 2 * n - 1; i >= 0; i--) {
+			int num = nums[i % n];
+			while (!stack.isEmpty() && stack.peek() <= num)
+				stack.pop();
+			if (!stack.isEmpty())
+				res[i % n] = stack.peek();
+			else
+				res[i % n] = -1;
+			stack.push(num);
+		}
+
+		return res;
+	}
+
+}
