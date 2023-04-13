d529a55087cfe10a067b062d1e8e722270d5ad42
vaseehar <vaseehar93@gmail.com>
2023-04-13 12:11:20 +0530

    13-4-23

---
+++ src/unknown/FooBar_1115.java
@@ -0,0 +1,39 @@
+package unknown;
+
+import java.util.concurrent.Semaphore;
+
+public class FooBar_1115 {
+
+	public static void main(String[] args) {
+
+	}
+
+	private int n;
+	Semaphore semFoo, semBar;
+
+	public FooBar_1115(int n) {
+		this.n = n;
+		semFoo = new Semaphore(1);
+		semBar = new Semaphore(0);
+	}
+
+	public void foo(Runnable printFoo) throws InterruptedException {
+
+		for (int i = 0; i < n; i++) {
+			semFoo.acquire();
+			printFoo.run();
+			semBar.release();
+		}
+
+	}
+
+	public void bar(Runnable printBar) throws InterruptedException {
+
+		for (int i = 0; i < n; i++) {
+			semBar.acquire();
+			printBar.run();
+			semFoo.release();
+		}
+	}
+
+}
