b5cd711aa0698c8a8f99ca7beb5c689737de7e33
vaseehar <vaseehar93@gmail.com>
2023-04-18 16:43:09 +0530

    16-4-23

---
+++ src/recursion/EmployeeImportance_690.java
@@ -0,0 +1,38 @@
+package recursion;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmployeeImportance_690 {
+
+	public static void main(String[] args) {
+	}
+
+	Map<Integer, Employee> employeeMap;
+
+	public int getImportance(List<Employee> employees, int id) {
+		if (employeeMap == null) {
+			employeeMap = new HashMap<>();
+			for (Employee emp : employees) {
+				employeeMap.put(emp.id, emp);
+			}
+		}
+		int imp = 0;
+		Employee emp = employeeMap.get(id);
+		imp += emp.importance;
+		if (emp.subordinates.size() > 0) {
+			for (int subId : emp.subordinates) {
+				imp += getImportance(employees, subId);
+			}
+		}
+		return imp;
+	}
+
+	class Employee {
+		public int id;
+		public int importance;
+		public List<Integer> subordinates;
+	};
+
+}
