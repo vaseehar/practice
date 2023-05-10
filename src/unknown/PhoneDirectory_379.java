bac4751b1cbd863d33b65ca473cdd4e7b7cfad24
vaseehar <vaseehar93@gmail.com>
2023-05-10 22:52:35 +0530

    10-5-23

---
+++ src/unknown/PhoneDirectory_379.java
@@ -0,0 +1,34 @@
+package unknown;
+
+import java.util.PriorityQueue;
+
+public class PhoneDirectory_379 {
+
+	PriorityQueue<Integer> available;
+	int maxSize;
+
+	public PhoneDirectory_379(int maxNumbers) {
+		available = new PriorityQueue<>(maxNumbers, (a, b) -> (a - b));
+		this.maxSize = maxNumbers;
+		for (int i = 0; i < maxNumbers; i++) {
+			available.add(i);
+		}
+	}
+
+	public int get() {
+		if (!available.isEmpty())
+			return available.poll();
+		else
+			return -1;
+	}
+
+	public boolean check(int number) {
+		return available.contains(number);
+
+	}
+
+	public void release(int number) {
+		if (!available.contains(number))
+			available.add(number);
+	}
+}
